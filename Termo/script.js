102aea6e4791c022fc3377bbefd25e44a61ec021
Ageu Moraes <ageumoraes67@gmail.com>
2024-06-12 22:43:17 -0300

    Subindo códigos do jogo

---
+++ Termo/script.js
@@ -0,0 +1,78 @@
+
+class TermoWords{
+
+    constructor(){
+        this.wordList = ["Turma"];
+        this.letterChoice = "";
+        this.letterPosition = "";
+    }
+
+    // Creats the "boxs" buttons
+    quantityButtons(quantityLines){
+        const divButton = document.querySelector(".divButtonCaixa");
+        const linelenght = 5;
+        let linesList = {};
+        // Runs the entire line
+        for (let i = 0; i < quantityLines; i++){
+            // Declare each "dictionary" item
+            linesList["line" + (i + 1)] = [];
+            // Loops through all columns creating all buttons text box 
+            for (let j = 0; j < linelenght; j++) {
+                let button = document.createElement("button");
+                button.textContent = ""
+                button.id = "button" + (i + 1) + (j + 1);
+                button.className = "button";
+                button.onclick = () => {
+                    alert("Você clicou no botão " + (i + 1) + (j + 1));
+                };
+                divButton.appendChild(button);
+                // Creating list by list using "dictionary" items
+                linesList["line" + (i + 1)].push("button" + (i + 1) + (j + 1));
+            }
+        } 
+        console.log(linesList);
+    }
+
+    // Creats key board
+    keyBoardButtons(){
+        const keyQtoP = document.querySelector("#key-q-to-p");
+        const keyAtoL = document.querySelector("#key-a-to-l");
+        const keyZtoM = document.querySelector("#key-z-to-m");
+
+        const listQp = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+        const listAl = ["A", "S", "D", "F", "G", "H", "J", "K", 'L'];
+        const listZm = ["Z" , "X" ,"C" ,"V" ,"B" ,"N" ,"M"];
+
+        this.putLetterOnScreen(listQp, keyQtoP);
+        this.putLetterOnScreen(listAl, keyAtoL);
+        this.putLetterOnScreen(listZm, keyZtoM);
+    }
+
+    // Puts the letters of key board on screen
+    putLetterOnScreen(list, fatherDiv){
+        for(let i = 0; i < list.length; i++){
+            let buttonLetter = document.createElement("button");
+            buttonLetter.textContent = list[i];
+            buttonLetter.id = list[i] + "button";
+            buttonLetter.className = "letter";
+            buttonLetter.onclick = () => {
+                alert(`Você clicou no botão: ${list[i]}`);
+                this.letterChoice = list[i];
+                this.buttonLetterClick();
+            };
+            fatherDiv.appendChild(buttonLetter);
+        }
+    }
+    
+    // Get clicked letter
+    buttonLetterClick(){
+        console.log(this.letterChoice);
+    }
+
+} 
+
+// Creating instance
+const termoWords = new TermoWords();
+// Functions of the class TermoWords
+termoWords.quantityButtons(6);
+termoWords.keyBoardButtons();
